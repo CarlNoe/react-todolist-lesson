8907ba7c18afa583ab8235b3c1589bd8ee1b101a
Noe Carl <noe.carldev@gmail.com>
2023-01-19 16:10:05 +0100

    todolist v1

--- src/TodoListBasic/TodoListBasic.tsx
+++ src/TodoListBasic/TodoListBasic.tsx
@@ -1,7 +1,86 @@
-import React from 'react';
+import { useState } from "react";
 
 const TodoListBasic = () => {
-    return <div>TodoListBasic</div>;
+  const [textFieldValue, setTextFieldValue] = useState<string>("");
+  const [currentCategory, setCurrentCategory] = useState<string>("todos");
+
+  const [todos, setTodos] = useState<string[]>([""]);
+  const [inProgress, setInProgress] = useState<string[]>([""]);
+  const [done, setDone] = useState<string[]>([""]);
+
+  //   todo: fix any
+  const handleOnChangeText = (event: any) => {
+    setTextFieldValue(event.target.value);
+  };
+
+  const handleOnChangeSelect = (e: any) => {
+    setCurrentCategory(e.target.value);
+  };
+
+  //   todo: fix any
+  const handleOnSubmit = (e: any) => {
+    e.preventDefault();
+
+    if (currentCategory === "todos") {
+      setTodos([...todos, textFieldValue]);
+    } else if (currentCategory === "inProgress") {
+      setInProgress([...inProgress, textFieldValue]);
+    } else if (currentCategory === "done") {
+      setDone([...done, textFieldValue]);
+    }
+  };
+
+  return (
+    <div style={{ padding: "50px" }}>
+      <form onSubmit={handleOnSubmit}>
+        <input onChange={handleOnChangeText} />
+
+        <select onChange={handleOnChangeSelect}>
+          <option value="todos">Todo</option>
+          <option value="inProgress">In Progress</option>
+          <option value="done">Done</option>
+        </select>
+
+        <button type="submit">Add To List</button>
+      </form>
+
+      <ul
+        id="todo-container"
+        style={{
+          display: "flex",
+          width: "50%",
+          justifyContent: "space-between",
+        }}
+      >
+        <li>
+          <h2 style={{ margin: "16px 0 16px 0" }}>Todo:</h2>
+          <div className="task-list">
+            {todos.map((todo) => (
+              <p>{todo}</p>
+            ))}
+          </div>
+        </li>
+
+        <li>
+          <h2 style={{ margin: "16px 0 16px 0" }}>In Progress::</h2>
+          <div className="task-list">
+            {inProgress.map((inProgressTask) => (
+              <p>{inProgressTask}</p>
+            ))}
+          </div>
+        </li>
+
+        <li>
+          <h2 style={{ margin: "16px 0 16px 0" }}>Done:</h2>
+          <div className="task-list">
+            {done.map((doneTask) => (
+              <p>{doneTask}</p>
+            ))}
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default TodoListBasic;
